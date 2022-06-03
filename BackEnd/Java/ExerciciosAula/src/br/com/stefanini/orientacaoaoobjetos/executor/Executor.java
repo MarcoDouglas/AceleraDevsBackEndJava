364cbbe2aa47f4239b99b4856175b352abfc7826
MarcoDouglas <marcomcd2@gmail.com>
2022-06-03 15:13:21 -0300

    Aula 8, Gerando Getters e Setters e Dois Construtores

--- BackEnd/Java/ExerciciosAula/src/br/com/stefanini/orientacaoaoobjetos/executor/Executor.java
+++ BackEnd/Java/ExerciciosAula/src/br/com/stefanini/orientacaoaoobjetos/executor/Executor.java
@@ -4,15 +4,20 @@ import br.com.stefanini.orientacaodeobjetos.Carro;
 
 public class Executor {
 	public static void main(String[] args) {
-		Carro carros1 = new Carro();
-		carros1.setMarca("BMW" );
-		//carros1.modelo = "320i";
-		//carros1.chassi = 9999;
-		//carros1.placa = "AAA99999"; 
-		//carros1.bancos = 5; 
-		//carros1.roda = 20;
+		Carro carro1 = new Carro();
+		carro1.setMarca("BMW" );
+		carro1.setModelo ("320i");
+		carro1.setChassi ( 9999);
+		carro1.setPlaca ("AAA99999"); 
+		carro1.setBancos(5); 
+		carro1.setRoda(20);
+		carro1.setVelocidade(280);
+		System.out.println(carro1.getMarca());
+		carro1.acelerar();
+		System.out.println(carro1.acelerar());
 		
-		System.out.println(carros1.getMarca());
+		Carro carro2=new Carro ("Mercedes");
+		System.out.println(carro2.getMarca());
 		
 	}
 }
--- BackEnd/Java/ExerciciosAula/src/br/com/stefanini/orientacaodeobjetos/Carro.java
+++ BackEnd/Java/ExerciciosAula/src/br/com/stefanini/orientacaodeobjetos/Carro.java
@@ -8,7 +8,29 @@ public class Carro {
 	private String placa;
 	private int roda;
 	private int bancos;
+	private int velocidade;
 	
+	public Carro() {
+		System.out.println("Construíndo um carro");
+	}
+	
+	public Carro(String marca) {
+		this.marca = marca;
+	}
+	
+	public String acelerar() {
+		return velocidade + "km/5s";
+	}
+	
+	
+	public int getVelocidade() {
+		return velocidade;
+	}
+
+	public void setVelocidade(int velocidade) {
+		this.velocidade = velocidade;
+	}
+
 	public String getMarca() {
 		return this.marca;
 	}
@@ -16,4 +38,45 @@ public class Carro {
 	public void setMarca(String marca) {
 		this.marca = marca;
 	}
+
+	public String getModelo() {
+		return modelo;
+	}
+
+	public void setModelo(String modelo) {
+		this.modelo = modelo;
+	}
+
+	public int getChassi() {
+		return chassi;
+	}
+
+	public void setChassi(int chassi) {
+		this.chassi = chassi;
+	}
+
+	public String getPlaca() {
+		return placa;
+	}
+
+	public void setPlaca(String placa) {
+		this.placa = placa;
+	}
+
+	public int getRoda() {
+		return roda;
+	}
+
+	public void setRoda(int roda) {
+		this.roda = roda;
+	}
+
+	public int getBancos() {
+		return bancos;
+	}
+
+	public void setBancos(int bancos) {
+		this.bancos = bancos;
+	}
+	
 }
