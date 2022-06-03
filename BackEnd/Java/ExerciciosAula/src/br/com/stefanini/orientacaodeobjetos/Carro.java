2d5767e7d84e6304d707123790b3b44d14052cec
MarcoDouglas <marcomcd2@gmail.com>
2022-06-03 14:30:48 -0300

    Aula 8, Criação de packages e acesso a atributos (public)

---
+++ BackEnd/Java/ExerciciosAula/src/br/com/stefanini/orientacaodeobjetos/Carro.java
@@ -0,0 +1,12 @@
+package br.com.stefanini.orientacaodeobjetos;
+
+public class Carro {
+	
+	public String marca;
+	public String modelo;
+	public int chassi;
+	public String placa;
+	public int roda;
+	public int bancos;
+	
+}
